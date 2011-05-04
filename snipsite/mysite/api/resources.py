e33bf7c620458ab3fc3a97b1db6f3d865a52ac6b
Jeff Boschee <jeff@mcloudlx.(none)>
2011-05-03 23:36:40 -0700

    More changes to models

--- snipsite/mysite/api/resources.py
+++ snipsite/mysite/api/resources.py
@@ -6,20 +6,26 @@ from tastypie import fields
 from tastypie.cache import SimpleCache
 from tastypie.resources import ModelResource
 from mysite.models import Snippet
+from tastypie.constants import ALL, ALL_WITH_RELATIONS
 
 class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
-        resource_name = 'auth/user'
+        resource_name = 'user'
         cache = SimpleCache()
         authentication = BasicAuthentication()
         authorization = DjangoAuthorization()
 
 class SnippetResource(ModelResource):
-    user = fields.ForeignKey(UserResource, 'author')
+    user = fields.ForeignKey(UserResource, 'user')
 	
     class Meta:
         queryset = Snippet.objects.all()
         list_allowed_methods = ['get', 'post']
-        detail_allowed_methods = ['get', 'post', 'put']
-
+        detail_allowed_methods = ['get', 'post', 'put', 'delete']
+        resource_name = 'snippet'
+        authorization = DjangoAuthorization()
+        filtering = {
+            "title": ALL,
+            "user": ('exact', 'startswith'),
+        }
--- snipsite/mysite/models.py
+++ snipsite/mysite/models.py
@@ -2,7 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 import datetime
 
-# Create your models here.
+PYTHON_VERSIONS = (
+    (3.0, '3.0'),
+    (2.7, '2.7'),
+    (2.6, '2.6'),
+    (2.5, '2.5'),
+    (0,  'Other'),
+)
 
 class Snippet(models.Model):
     title = models.CharField(max_length=255)
@@ -10,6 +16,14 @@ class Snippet(models.Model):
     description = models.TextField()
     description_html = models.TextField(editable=False)
     code = models.TextField()
+    highlighted_code = models.TextField(editable=False)
+    python_version = models.FloatField(choices=PYTHON_VERSIONS, default=2.7)
     pub_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    rating_score = models.IntegerField(default=0)
     
+    def __unicode__(self):
+        return self.title
+
+    def entered_today(self):
+        return self.pub_date.date() == datetime.date.today()
